eb2c0191b6e58a4402758e9a55a3cc96172152b5
Any Medola <anynhamedola@gmail.com>
2023-05-22 12:06:37 +0200

    Update removeAccent.js

--- removeAccent.js
+++ removeAccent.js
@@ -1,5 +1,12 @@
-function removeAccent(str) {
-    // Add your solution here!
+function removeAccent(str) {  
+    str = str.toLowerCase();                                                     
+    str = str.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    str = str.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    str = str.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    str = str.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    str = str.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    str = str.replace(new RegExp('[Ç]','gi'), 'c');
+    return str;           
 }
 
 module.exports = removeAccent;
